7c8930f5437b5f2713541a71bd379a0a3c2db3c8
bonduSeo <bondu.seo@gmail.com>
2022-07-21 23:21:44 +0900

    강의끝

---
+++ src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" name="" id="" v-bind:value="userId" readonly />
+    <!-- value라는 속성에 직접 데이터 바인딩함. 단방향 데이터 바인딩 -->
+    <input type="text" name="" id="" :value="userId" readonly />
+    <!-- 같은의미 -->
+    <br />
+    <img :src="imgSrc" alt="" style="width: 100px; height: auto" />
+    <br />
+    <input type="search" name="" id="" v-model="txt1" />
+    <button :disabled="txt1 === ''">조회</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'bondu',
+      imgSrc:
+        'https://4.bp.blogspot.com/-o4RyOEhoHo8/WZVribxbufI/AAAAAAAABMk/0ZUqq5lpNp0H636rIbKgcVPE51gf51AaQCLcBGAs/s1600/adventure-time.png',
+      txt1: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ src/views/2_event/EventKeyView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- <input
+      type="search"
+      name=""
+      id=""
+      @keyup="checkEnter($event)"
+      v-model="searchText"
+    /> -->
+    <input
+      type="search"
+      name=""
+      id=""
+      @keyup.enter="doSearch"
+      v-model="searchText"
+    />
+    <button @click="doSearch">조회</button>
+    <button type="submit" @click.prevent="doSearch"></button>
+  </div>
+</template>
+<script>
+// .enter
+// .tap .delete .esc .space .up .down .left .right가능
+// @keyup.ctrl.enter 이렇게도가능
+// .stop 은 event.stopPropagation() (버블링/캡쳐링 방지)
+// .prevent 는 event.preventDefault()
+export default {
+  components: {},
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searchText)
+    },
+    checkEnter(event) {
+      if (event.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>
